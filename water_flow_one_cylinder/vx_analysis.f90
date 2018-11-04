f7188ac17c987aabcf8d05e6816bbea65a2d6658
Anders Johansson <anjohan@uio.no>
2018-11-04 15:23:46 +0100

    New analysis of binary outputs from own fix.
    Something wrong with end values, otherwise seems to work. Averaging
    should be more correct with this than with previous analysis of .profile
    files.

--- water_flow_one_cylinder/vx_analysis.f90
+++ water_flow_one_cylinder/vx_analysis.f90
@@ -1,104 +1,175 @@
-program vx_analysis
-    use iso_c_binding, only: c_int, c_long_long, c_double
-    use iso_fortran_env, only: iostat_end
+program vx_profile_analysis
+    use iso_fortran_env, only: iostat_end, real64, int64
     implicit none
-    character(len=1024) :: infile, outfile
-    integer(c_int) :: num_chunks, num_columns, values_in_chunk, atoms_in_chunk
-    integer(c_long_long) :: num_atoms, timestep, start_timestep
 
-    integer(c_int) :: triclinic, boundary_conditions(2,3)
-    real(c_double) :: boundary(2,3), angles(3)
+    character(len=1024) :: inbase, Fstring, Ffmt, outbase
+    character(len=:), allocatable :: filename, fmtstring
 
-    real(c_double), allocatable :: atom_data(:,:), vx_hist(:)
-    integer(c_int), allocatable :: count_hist(:)
-    integer(c_int) :: num_bins
-    real(c_double) :: bin_size = 0, y_fraction, Lx, Ly, ymin, ymax
+    integer :: i, j, k, l, u
 
-    integer :: u, i, j, fstat = 0
-    integer :: x_idx, vx_idx, bin_idx
+    integer(int64) :: step
+    integer :: Nx, Ny, Nz, fstat
+    real(real64) :: F, ymin, ymax, zmin, zmax, xmin, xmax,  radius
+    real(real64), allocatable :: v(:,:,:,:,:), values(:,:,:,:), Fs(:), &
+                                 counts(:,:,:,:)
 
-    namelist /input/ infile, start_timestep, outfile, y_fraction, num_bins, x_idx, vx_idx
-    read(*,nml=input)
+    real(real64) :: y_fraction, Lx, Ly, Fmin, Fstep
+    integer :: numFs, start_timestep, ave_x
 
-    allocate(vx_hist(num_bins), count_hist(num_bins), atom_data(1,1))
-    vx_hist(:) = 0
-    count_hist(:) = 0
+    namelist /input/ inbase, start_timestep, outbase, y_fraction, radius, &
+                     Fmin, Fstep, numFs, Ffmt, ave_x
 
-    open(newunit=u, file=trim(infile), access="stream", action="read", iostat=fstat)
-    if (fstat /= 0) then
-        write(*,*) "Cannot open " // infile
-        error stop
+    if (this_image() == 1) then
+        read(*, nml=input)
     end if
 
-    steploop: do
-        read(u, iostat=fstat) timestep, num_atoms, triclinic, boundary_conditions, boundary
-        if (fstat == iostat_end) then
-            exit steploop
+    call co_broadcast(inbase, 1)
+    call co_broadcast(start_timestep, 1)
+    call co_broadcast(outbase, 1)
+    call co_broadcast(y_fraction, 1)
+    call co_broadcast(radius, 1)
+    call co_broadcast(Fmin, 1)
+    call co_broadcast(Fstep, 1)
+    call co_broadcast(numFs, 1)
+    call co_broadcast(Ffmt, 1)
+    call co_broadcast(ave_x, 1)
+
+    Fs = [(Fmin + i*Fstep, i = 0, numFs-1)]
+
+    forces: do l = 1, numFs
+        if (mod(l-1, num_images()) /= this_image()-1) cycle forces
+        F = Fs(l)
+        Fstring(:) = " "
+        write(Fstring, fmt=Ffmt) F
+
+        filename = trim(inbase) // trim(Fstring) // ".bin"
+
+        open(newunit=u, file=filename, access="stream", status="old")
+
+        read(u) Nx, Ny, Nz, xmin, xmax, ymin, ymax, zmin, zmax
+        if (.not. allocated(values)) allocate(values(4, Nz, Ny, Nx))
+        if (.not. allocated(counts)) then
+            allocate(counts(Nz, Ny, Nx, numFs))
+            counts(:,:,:,:) = 0
         end if
 
-        write(*,*) "Reading timestep", timestep
-
-        if (triclinic > 0) then
-            read(u, iostat=fstat) angles
-            if (fstat == iostat_end) then
-                exit steploop
-            end if
+        if (.not. allocated(v)) then
+            allocate(v(3, Nz, Ny, Nx, numFs))
+            v(:,:,:,:,:) = 0
         end if
 
-        if (bin_size == 0) then
-            Lx = boundary(2,1) - boundary(1,1)
-            Ly = boundary(2,2) - boundary(1,2)
-            bin_size = Lx/num_bins
-            ymin = (0.5-y_fraction/2)*Ly
-            ymax = (0.5+y_fraction/2)*Ly
-        end if
+        steps: do
+            read(u, iostat=fstat) step
 
-        read(u, iostat=fstat) num_columns, num_chunks
-        if (fstat == iostat_end) then
-            exit steploop
-        end if
+            if (fstat == iostat_end) exit steps
+            if (fstat /= 0) error stop
 
-        chunkloop: do i = 1, num_chunks
-            read(u, iostat=fstat) values_in_chunk
-            if (fstat == iostat_end) then
-                exit steploop
-            end if
-            atoms_in_chunk = values_in_chunk/num_columns
+            read(u, iostat=fstat) values
+            if (fstat /= 0) error stop
 
-            if(size(atom_data,2) < atoms_in_chunk) then
-                deallocate(atom_data)
-                allocate(atom_data(num_columns, atoms_in_chunk))
-            end if
+            if (step < start_timestep) cycle steps
 
-            read(u, iostat=fstat) atom_data(:,1:atoms_in_chunk)
-            if (fstat == iostat_end) then
-                exit steploop
-            end if
+            write(*,"(a,x,i0,x,a,x,i0,x,a)") "Image", this_image(), "reading timestep", &
+                                             step, "from " // filename
 
-            if (timestep < start_timestep) cycle chunkloop
+            counts(:,:,:,l) = counts(:,:,:,l) + values(1,:,:,:)
 
-            atomloop: do j = 1, atoms_in_chunk
-                associate(x  => atom_data(x_idx, j), &
-                          y  => atom_data(x_idx+1, j), &
-                          vx => atom_data(vx_idx, j))
-                    if (y < ymin .or. y > ymax) cycle atomloop
-                    bin_idx = int(x/bin_size) + 1
-                    bin_idx = min(bin_idx, num_bins)
-                    bin_idx = max(bin_idx, 1)
-                    count_hist(bin_idx) = count_hist(bin_idx) + 1
-                    vx_hist(bin_idx) = vx_hist(bin_idx) + vx
-                end associate
-            end do atomloop
-        end do chunkloop
-    end do steploop
-    where(count_hist > 0)
-        vx_hist = vx_hist/count_hist
-    end where
-    close(u)
+            do i = 1, Nx
+                do j = 1, Ny
+                    do k = 1, Nz
+                        v(:, k, j, i, l) = v(:, k, j, i, l) + values(1, k, j, i) * values(2:4, k, j, i)
+                    end do
+                end do
+            end do
+        end do steps
 
-    open(newunit=u, file=trim(outfile), status="replace")
-    do i = 1, num_bins
-        write(u,*) (i-0.5d0)*bin_size, vx_hist(i), abs((i-0.5d0)*bin_size-Lx/2)
-    end do
-    close(u)
+        do i = 1, Nx
+            do j = 1, Ny
+                do k = 1, Nz
+                if (counts(k,j,i,l) /= 0) v(:, k,j,i,l) = v(:, k,j,i,l) &
+                                                           / counts(k,j,i,l)
+                end do
+            end do
+        end do
+    end do forces
+    call co_sum(v)
+    call co_sum(counts)
+
+    deallocate(values)
+    if (this_image() /= 1) deallocate(v, counts)
+
+    if (this_image() == 1) then
+    block
+        real(real64) :: dx, dy ,dz, tmp
+        integer :: start_bin_asymm, ymin_idx, ymax_idx
+        real(real64), allocatable :: vx_ave_z(:,:,:), vx_ave_yz(:,:), &
+                                     vx_front(:,:), vx_rear(:,:), counts_ave_z(:,:,:)
+
+        dx = (xmax - xmin)/Nx
+        dy = (ymax - ymin)/Ny
+        dz = (zmax - zmin)/Nz
+
+        start_bin_asymm = ceiling(radius/dx)
+        ymin_idx = floor((0.5d0 - y_fraction/2)*(ymax-ymin)/dy)
+        ymax_idx = floor((0.5d0 + y_fraction/2)*(ymax-ymin)/dy)
+
+        allocate(vx_ave_z(Ny,Nx,numFs), &
+                 vx_ave_yz(Nx, numFs))
+
+        counts_ave_z = sum(counts(:,:,:,:), dim=1)/Nz
+
+        where (counts_ave_z /= 0)
+            vx_ave_z = sum(v(1,:,:,:,:)*counts(:,:,:,:), dim=1)/counts_ave_z
+        else where
+            vx_ave_z = 0
+        end where
+
+
+        do l = 1, numFs
+            do i = 1, Nx
+                tmp = sum(counts_ave_z(ymin_idx:ymax_idx,i,l))
+                if (tmp == 0) then
+                    vx_ave_yz(i, l) = 0
+                    cycle
+                end if
+
+                vx_ave_yz(i, l) = sum(vx_ave_z(ymin_idx:ymax_idx,i,l) &
+                                      * counts_ave_z(ymin_idx:ymax_idx,i,l)) &
+                                / tmp
+            end do
+        end do
+
+        vx_front = vx_ave_yz(Nx/2-start_bin_asymm:1:-1, :)
+        vx_rear = vx_ave_yz(Nx/2+1+start_bin_asymm:, :)
+        open(newunit=u, file=trim(outbase)//"_vx_vs_x.dat", status="replace")
+        fmtstring = "('x ',*('F='," // Ffmt // ",'eV/Å',:,x))"
+        write(u, fmt=fmtstring) Fs
+        do i = 1, Nx
+            write(u, fmt="(*(f0.6,:,x))") (i-1)*dx, (vx_ave_yz(i,j), j = 1, numFs)
+        end do
+        close(u)
+
+        open(newunit=u, file=trim(outbase)//"_vx_vs_x_wrapped.dat", status="replace")
+        fmtstring = "('x ',*('F='," // Ffmt // ",'eV/Å,infront',x,'F='," &
+            // Ffmt // ",'eV/Å,behind',:,x))"
+        write(u, fmt=fmtstring) ([Fs(i),Fs(i)] , i = 1, numFs)
+        do i = 1, size(vx_front, 1)
+            write(u, fmt="(*(f0.6,:,x))") radius+(i-1)*dx, &
+                                          (vx_rear(i,j), &
+                                           vx_front(i,j), j = 1, numFs)
+        end do
+        close(u)
+
+        open(newunit=u, file=trim(outbase) // "_asymmetry.dat", status="replace")
+        write(u, *) "F[eV/Å] <vx>[Å/ps] relative_2norm_diff relative_integral_diff"
+        do i = 1, numFs
+            write(u, *) Fs(i), &
+                sum(vx_front(:,i) + vx_rear(:,i)) / (size(vx_front(:,i)) + size(vx_rear(:,i))), &
+                norm2(vx_front(:,i) - vx_rear(:,i)) / norm2(vx_rear(:,i)), &
+                sum(vx_front(:,i) - vx_rear(:,i)) / sum(vx_rear(:,i))
+            write(*,*) norm2(vx_rear(:,i)), sum(vx_rear(:,i))
+        end do
+        close(u)
+    end block
+    end if
 end program
