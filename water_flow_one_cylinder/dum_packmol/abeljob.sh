c47dd96b048aba387901ad7e688388129f511955
Anders Johansson <anjohan@uio.no>
2019-02-01 14:30:20 +0100

    attempt at temp loop in cooling

--- water_flow_one_cylinder/dum_packmol/abeljob.sh
+++ water_flow_one_cylinder/dum_packmol/abeljob.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
-# Number of tasks (MPI ranks):
-#SBATCH --time='6-00:00:00'
-# #SBATCH --account=nn9272k
-#SBATCH --nodes=20
+#SBATCH --time='0-00:30:00'
 #SBATCH --account=trocks
-# #SBATCH --nodes=5
-#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=anjohan@uio.no
 #SBATCH --mem-per-cpu=3600M
-source /cluster/bin/jobsetup
-module load intelmpi.intel
-module load intel
-module load python3
 
-make
+# source /cluster/bin/jobsetup
+module load intel
+mpirun -n 1 ls
+#
+# mpirun -n 1 /usit/abel/u1/anjohan/cmake_lammps/lammps/build/lmp -in make_amorphous.in
+# make data/amorphous.data
