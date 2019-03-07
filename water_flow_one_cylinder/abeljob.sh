56b2ab5e9a93d314263824c7c23336e5ad9fa92e
Anders Johansson <anjohan@uio.no>
2019-03-07 10:47:56 +0100

    seems to work with only oxygen sampling

--- water_flow_one_cylinder/abeljob.sh
+++ water_flow_one_cylinder/abeljob.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#SBATCH --time='0-00:30:00'
-#SBATCH --account=trocks
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --time='3-00:00:00'
+#SBATCH --account=nn9272k
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=anjohan@uio.no
 #SBATCH --mem-per-cpu=3600M
 
 source /cluster/bin/jobsetup
-module load intel/2019.2
+module load intel/2018.1
 module load intelmpi.intel
 
-mpirun -n 1 /usit/abel/u1/anjohan/cmake_lammps/lammps/build/lmp -in make_amorphous.in
+$HOME/miniconda3/bin/make ITERATIONS=10
